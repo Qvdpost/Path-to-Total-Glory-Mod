1d7e964c4944106762d10e4ac3664d423c5a4062
Quinten <qvdpost@gmail.com>
2024-04-22 15:02:46 +0200

    removed redundant setup

--- script/_lib/mod/pttg_setup.lua
+++ script/_lib/mod/pttg_setup.lua
@@ -3,21 +3,7 @@ local mct = get_mct();
 local pttg_upkeep = core:get_static_object("pttg_upkeep")
 
 
-local function restrict_player()
-    core:add_listener(
-        "award_random_magical_item",
-        "TriggerPostBattleAncillaries",
-        true,
-        function(context)
-            return false;
-        end,
-        true
-    );
-end
-
 local function init() 
 end
-
-cm:add_first_tick_callback(function() restrict_player() end);
 
 cm:add_first_tick_callback(function() init() end);

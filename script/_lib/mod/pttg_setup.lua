21572720dbf55c86f2412560f8caae35aae1e3f6
Quinten <qvdpost@gmail.com>
2024-04-22 14:59:29 +0200

    removed redundant setup

--- script/_lib/mod/pttg_setup.lua
+++ script/_lib/mod/pttg_setup.lua
@@ -15,28 +15,7 @@ local function restrict_player()
     );
 end
 
--- TODO: find a better place for this
-local function zero_merc_cost()
-    local recruitment_cost_bundle_key = "pttg_merc_recruit_cost_down";
-	local faction = cm:get_local_faction()
-
-    if faction:has_effect_bundle(recruitment_cost_bundle_key) then
-        return
-    end
-
-    local recruitment_cost_bundle = cm:create_new_custom_effect_bundle(recruitment_cost_bundle_key);
-    
-    recruitment_cost_bundle:add_effect("wh3_main_effect_mercenary_cost_mod", "faction_to_character_own_factionwide_armytext", -10000);
-    recruitment_cost_bundle:set_duration(0);
-
-    recruitment_cost_bundle:add_effect("wh_main_effect_force_all_campaign_recruitment_cost_all", "faction_to_character_own_factionwide_armytext", -10000);
-    recruitment_cost_bundle:set_duration(0);
-
-    cm:apply_custom_effect_bundle_to_faction(recruitment_cost_bundle, faction);
-end
-
 local function init() 
-    pttg_upkeep:add_callback("pttg_zero_merc_cost", zero_merc_cost, nil)
 end
 
 cm:add_first_tick_callback(function() restrict_player() end);
